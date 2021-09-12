63e852bb12e37d3c1c23f55e4c696f04b658a09d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-12 20:46:57 +0530

    added Account

---
+++ src/main/java/com/gb/lld/linkedin/member/Account.java
@@ -0,0 +1,12 @@
+package com.gb.lld.linkedin.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Account {
+    private String email;
+    private String phone;
+    private String password;
+}
