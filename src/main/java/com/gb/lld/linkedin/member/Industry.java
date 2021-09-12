b374f0fd59db9743f779e14577b453990ff40918
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-12 20:41:58 +0530

    added Industry enum

---
+++ src/main/java/com/gb/lld/linkedin/member/Industry.java
@@ -0,0 +1,12 @@
+package com.gb.lld.linkedin.member;
+
+public enum Industry {
+    RETAIL,
+    MANUFACTURING,
+    HEALTH_CARE,
+    IT_SOFTWARE,
+    IT_HARDWARE,
+    CONSTRUCTION,
+    HOSPITALITY,
+    SERVICES,
+}
