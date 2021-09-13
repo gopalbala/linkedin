cb75a1bb938df33c4823990f161a457dc0c83d85
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-14 01:25:49 +0530

    added Education

---
+++ src/main/java/com/gb/lld/linkedin/member/Education.java
@@ -0,0 +1,14 @@
+package com.gb.lld.linkedin.member;
+
+import java.time.LocalDate;
+
+public class Education {
+    private String school;
+    private String degree;
+    private String fieldOfStudy;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String grade;
+    private String activities;
+    private String description;
+}
