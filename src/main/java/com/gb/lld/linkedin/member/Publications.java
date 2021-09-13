aa63d92f34e47b2d46c97510f3f70adc5ec8ae5e
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-14 01:45:37 +0530

    added Publications

---
+++ src/main/java/com/gb/lld/linkedin/member/Publications.java
@@ -0,0 +1,16 @@
+package com.gb.lld.linkedin.member;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class Publications {
+    private String id;
+    private String title;
+    private String publisher;
+    private String publicationDate;
+    private String author;
+    private String url;
+    private String description;
+}
