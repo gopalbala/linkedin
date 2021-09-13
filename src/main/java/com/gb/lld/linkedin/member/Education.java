d9b9d36f939d1769fe43f2d7137d207633f474d9
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-14 01:28:03 +0530

    updated Education and added ProfileUpdater

--- src/main/java/com/gb/lld/linkedin/member/Education.java
+++ src/main/java/com/gb/lld/linkedin/member/Education.java
@@ -1,8 +1,14 @@
 package com.gb.lld.linkedin.member;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
 public class Education {
+    private String educationId;
     private String school;
     private String degree;
     private String fieldOfStudy;
--- src/main/java/com/gb/lld/linkedin/member/ProfileUpdater.java
+++ src/main/java/com/gb/lld/linkedin/member/ProfileUpdater.java
@@ -1,6 +1,8 @@
 package com.gb.lld.linkedin.member;
 
-public interface Experiences {
+public interface ProfileUpdater {
     Experience addExperience(Experience experience);
     void removeExperience(int experienceId);
+    Education addEduction(Education education);
+    void removeEducation(int educationId);
 }
