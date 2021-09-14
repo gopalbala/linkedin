23d11f9a821a3cbb4e513cecdc446b24c1faba82
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-15 00:00:28 +0530

    added Cause

---
+++ src/main/java/com/gb/lld/linkedin/member/Cause.java
@@ -0,0 +1,11 @@
+package com.gb.lld.linkedin.member;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Cause {
+    private String id;
+    private String cause;
+}
