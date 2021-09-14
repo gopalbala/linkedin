5adc9b4445605201b973fa2b8b23800117109524
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-15 00:02:27 +0530

    modified AccomplishmentUpdater

--- src/main/java/com/gb/lld/linkedin/member/AccomplishmentUpdater.java
+++ src/main/java/com/gb/lld/linkedin/member/AccomplishmentUpdater.java
@@ -3,4 +3,9 @@ package com.gb.lld.linkedin.member;
 public interface AccomplishmentUpdater {
     Publication addPublication(Publication publication);
     void removePublication(String publicationId);
+    Publication updatePublication(Publication publication);
+
+    Patent addPatent(Patent patent);
+    void removePatent(String patentId);
+    Patent updatePatent(Patent patent);
 }
