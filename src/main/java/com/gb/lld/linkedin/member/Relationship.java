51c9d2ea562ca5e434a789544bd2b0cbe6f84e4e
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-15 00:01:04 +0530

    added Relationship

---
+++ src/main/java/com/gb/lld/linkedin/member/Relationship.java
@@ -0,0 +1,14 @@
+package com.gb.lld.linkedin.member;
+
+public enum Relationship {
+    MANAGED_ME_DIRECTLY,
+    REPORTED_TO_ME,
+    SENIOR_TO_ME,
+    I_AM_SENIOR,
+    WORKED_WITH,
+    CLIENT,
+    MENTOR,
+    TEACHER,
+    COLLEAGUE,
+    STUDIED_TOGETHER
+}
