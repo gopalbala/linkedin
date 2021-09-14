d4483390f73233080f0c7da16e95f3910401405d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-15 00:01:39 +0530

    modified BackgroundUpdater

--- src/main/java/com/gb/lld/linkedin/member/BackgroundUpdater.java
+++ src/main/java/com/gb/lld/linkedin/member/BackgroundUpdater.java
@@ -3,10 +3,17 @@ package com.gb.lld.linkedin.member;
 public interface BackgroundUpdater {
     Experience addExperience(Experience experience);
     void removeExperience(String experienceId);
+    Experience updateExperience(Experience experience);
+
     Education addEduction(Education education);
     void removeEducation(String educationId);
+    Education updateEduction(Education education);
+
     License addLicense(License license);
     void removeLicense(String licenseId);
+    License updateLicense(License license);
+
     VolunteerExperience addVolunteerExperience(VolunteerExperience volunteerExperience);
     void  removeVolunteerExperience(String volunteerExperienceId);
+    VolunteerExperience updateVolunteerExperience(VolunteerExperience volunteerExperience);
 }
