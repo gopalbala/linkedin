f68cbeaa07aca2576eaddbfdb0b0fca1e6845dd5
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-14 23:39:44 +0530

    added AccomplishmentUpdater.java

---
+++ src/main/java/com/gb/lld/linkedin/member/AccomplishmentUpdater.java
@@ -0,0 +1,6 @@
+package com.gb.lld.linkedin.member;
+
+public interface AccomplishmentUpdater {
+    Publication addPublication(Publication publication);
+    void removePublication(String publicationId);
+}
