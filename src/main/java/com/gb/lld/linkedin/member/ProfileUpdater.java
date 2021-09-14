670bf42140c4158fb7c665045ddda93feeb4952a
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2021-09-14 23:39:24 +0530

    updated ProfileUpdater

--- src/main/java/com/gb/lld/linkedin/member/ProfileUpdater.java
+++ src/main/java/com/gb/lld/linkedin/member/ProfileUpdater.java
@@ -1,18 +1,8 @@
 package com.gb.lld.linkedin.member;
 
-import java.util.List;
-
 public interface ProfileUpdater {
     Experience addExperience(Experience experience);
     void removeExperience(String experienceId);
     Education addEduction(Education education);
     void removeEducation(String educationId);
-    License addLicense(License license);
-    void removeLicense(String licenseId);
-    VolunteerExperience addVolunteerExperience(VolunteerExperience volunteerExperience);
-    void  removeVolunteerExperience(String volunteerExperienceId);
-    Skill addSkill(Skill skill);
-    void removeSkill(String skillId);
-    Publication addPublication(Publication publication);
-    void removePublication(String publicationId);
 }
